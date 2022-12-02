7d14fbc8c081a6187815d22140816d116d805330
DiegoEstela <die.estela@gmail.com>
2022-12-02 16:13:39 +0100

    start route

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,8 @@
 export default function HomePage () {
-  return <h1> App con Nextjs 13 </h1>
+  return (
+    <div>
+      <h1> App con Nextjs 13 </h1>
+      <p> Prueba de rutas con NextJs</p>
+    </div>
+  )
 }
